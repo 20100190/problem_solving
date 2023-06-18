e65e8d4ac821757e26a54356dd10673ec6c9ce3f
Usman <20100190@lums.edu.pk>
2023-06-18 14:33:28 +0500

    adding Bubble sort O^2

--- list_sort.py
+++ list_sort.py
@@ -5,7 +5,6 @@ def list_sort(nums):
     """
     # check if first element is smallest? 
         # if not replace it with next smallest number
-
     for _ in nums:
         for index, element in enumerate(nums):
             for j,i in enumerate(nums[index+1:]):
@@ -15,13 +14,27 @@ def list_sort(nums):
                 break
     return nums
 
-    
+def bubble_sort(my_list):
+    """
+    :type nums: List[int]
+    rtype: List[int]
+    """
+    for _ in my_list:
+        for index in range(len(my_list)-1):
+            if my_list[index] > my_list[index+1]:
+                temp = my_list[index]
+                my_list[index] = my_list[index+1]
+                my_list[index+1] = temp
+    return my_list
+
 def main():
     nums = [8,9,5,1,6,2,9,11]
     nums2 = [1,2,3,4]
     nums3 = [4,3,2,1]
-    index = list_sort(nums=nums2)
+    index = list_sort(nums=nums)
+    index2 = bubble_sort(my_list=nums)
     print(index)
+    print(index2)
 
 if __name__ == "__main__":
     main()
